a8aa61bdf5b55b11686a505fd2713174cca711c6
AyoubHamou <ahamou@student.1337.ma>
2025-06-05 14:41:00 +0100

    changed the ratelimit

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -3,11 +3,11 @@ import { Ratelimit } from '@upstash/ratelimit'
 import dotenv from 'dotenv'
 
 dotenv.config();
-//only allow 10 requests per hour
+//only allow 20 requests per hour
 
 const rateLimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter:Ratelimit.slidingWindow(20, "1 h")
+    limiter:Ratelimit.slidingWindow(100, "1 h")
 })
 
 export default rateLimit
